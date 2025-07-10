a42046c1298f3323052abae58b1f458b92e75ebb
Fabigom9603 <edison.gomez03@uptc.edu.co>
2025-07-10 15:15:29 -0500

    PuzzleRompecabezas
    -Script Puzzle

---
+++ Assets/Scripts/PuzzleRompecabezas/PuzzleManager.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class PuzzleManager : MonoBehaviour
+{
+    public List<Transform> tiles = new List<Transform>(); // Asigna los cubos en el Inspector
+    public Transform emptySpot; // El espacio vacío
+    public float moveDuration = 0.2f;
+
+    public void TryMoveTile(Transform tile)
+    {
+        Vector3 localTilePos = tile.localPosition;
+        Vector3 localEmptyPos = emptySpot.localPosition;
+
+        if (Vector3.Distance(localTilePos, localEmptyPos) < 1.1f)
+        {
+            StartCoroutine(MoveTile(tile, localEmptyPos));
+            emptySpot.localPosition = localTilePos;
+        }
+    }
+
+    IEnumerator MoveTile(Transform tile, Vector3 targetLocalPos)
+    {
+        float elapsed = 0f;
+        Vector3 start = tile.localPosition;
+
+        while (elapsed < moveDuration)
+        {
+            tile.localPosition = Vector3.Lerp(start, targetLocalPos, elapsed / moveDuration);
+            elapsed += Time.deltaTime;
+            yield return null;
+        }
+
+        tile.localPosition = targetLocalPos;
+    }
+}
